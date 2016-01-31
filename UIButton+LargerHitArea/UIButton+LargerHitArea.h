f139af5ff865e26bd62bd31573c8500278187839
drops <drops@DropsMacBook-Pro.local>
2016-01-31 16:36:38 +0800

    commit the category

---
+++ UIButton+LargerHitArea/UIButton+LargerHitArea.h
@@ -0,0 +1,15 @@
+//
+//  UIButton.h
+//  ButtonLargerHitArea
+//
+//  Created by drops on 16/1/31.
+//  Copyright © 2016年 TXW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIButton(LargerHitArea)
+
+@property (nonatomic, assign) UIEdgeInsets hitEdgeInsets;
+
+@end
